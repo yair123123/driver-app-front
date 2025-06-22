f82ded9d0962525d25c15cb1f4238c58e839c0a1
DELL <ytykhn8@gmail.com>
2025-06-22 17:01:38 +0300

    update config.js

--- web/assets/config.js
+++ web/assets/config.js
@@ -1,4 +1,6 @@
+// config.js
 window.env = {
-    "API_AUTH_URL": "localhost:5000",
-    "WEBSOCKET_RIDE_SERVICE": "ws://localhost:12345",
-}
+API_AUTH_URL:"main-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
+API_RIDES_URL:"http://127.0.0.1:5050/api/rides",
+"WEBSOCKET_RIDE_SERVICE": "ws://localhost:12345",
+};
