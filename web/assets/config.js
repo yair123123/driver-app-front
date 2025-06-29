0451a57d629857b5373a053c6a523343fb44d065
Yair <ytykhn8@gmail.com>
2025-06-29 07:43:06 -0700

    fix problems after rebase

--- web/assets/config.js
+++ web/assets/config.js
@@ -1,5 +1,10 @@
+// // config.js
+// window.env = {
+// API_AUTH_URL:"main-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
+// WEBSOCKET_RIDE_SERVICE: "ws://ride-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
+// };
 // config.js
 window.env = {
-API_AUTH_URL:"main-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
-WEBSOCKET_RIDE_SERVICE: "ws://ride-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
-};
+    API_AUTH_URL: "localhost:5000",
+    WEBSOCKET_RIDE_SERVICE: "ws://localhost:12345",
+}
