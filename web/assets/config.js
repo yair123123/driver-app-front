49ff33847b06d61224d5e1842e780b2d1696fde0
Yair <ytykhn8@gmail.com>
2025-07-03 06:45:04 -0700

    match android

--- web/assets/config.js
+++ web/assets/config.js
@@ -4,5 +4,5 @@ window.env =
 
 {
   API_AUTH_URL: "main-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
-  WEBSOCKET_RIDE_SERVICE: "ws://ride-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  WEBSOCKET_RIDE_SERVICE: "wss://ride-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
 };
