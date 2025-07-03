7e0cc135c85ebaeed929156256c2f43e0c66fa48
Yair <ytykhn8@gmail.com>
2025-07-03 01:14:39 -0700

    update config.js

--- web/assets/config.js
+++ web/assets/config.js
@@ -1,10 +1,8 @@
-// // config.js
-// window.env = {
-// API_AUTH_URL:"main-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
-// WEBSOCKET_RIDE_SERVICE: "ws://ride-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
-// };
 // config.js
-window.env = {
-    API_AUTH_URL: "localhost:5000",
-    WEBSOCKET_RIDE_SERVICE: "ws://localhost:12345",
-}
+
+window.env =
+
+{
+  API_AUTH_URL: "main-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
+  WEBSOCKET_RIDE_SERVICE: "ws://ride-route-yizwshe-dev.apps.rm2.thpm.p1.openshiftapps.com",
+};
